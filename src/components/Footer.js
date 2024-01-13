75637cf9dbbc56f41a16d19879f295c796fc8f6b
immortalsobject <immortalsobject@gmail.com>
2024-01-13 08:47:45 +0700

    ex8

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-export default class Footer extends Component{
- render(){
+export default function Footer (){
+
     return (
     <div className='footer'>
         <h3>Copy right @2024</h3>
     </div>
         )
  }
-}
+
 
