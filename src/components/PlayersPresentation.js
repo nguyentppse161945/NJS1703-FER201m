70ec1ac7c0e5813df10e0d4f84c32b97db8af74e
nguyentppse161945 <immortalsobject@gmail.com>
2024-01-14 16:06:04 +0700

    ex10

--- src/components/PlayersPresentation.js
+++ src/components/PlayersPresentation.js
@@ -1,20 +1,39 @@
-import React from 'react'
-
-export default function PlayersPresentation({players}) {
-    console.log(players);
+import React, { useState } from "react";
+import PlayersList from "../Share/ListOfPlayers";
+export default function PlayersPresentation({ players }) {
+  const [player, setPlayer] = useState([]);
   return (
-    <div className='container'>
-        {players.map((player)=>(
-            <div className='column'>
-                <div className='card'>
-                    <img src={player.img}/>
-                    <h3>{player.name}</h3>
-                    <p className='title'>{player.club}</p>
-                    <p><button>Detail</button></p>
-                </div>
-            </div>
-        ))}
+    <div className="container">
+      {PlayersList.map((player) => (
+        <div className="column" key={player.id}>
+          <div className="card">
+            <img src={player.img} />
+            <h3>{player.name}</h3>
+            <p className="tittle">{player.club}</p>
+            <p>
+              <button
+                onClick={() => {
+                  setPlayer(player);
+                }}
+              >
+                <a href="#popup1" id="openPopUp">
+                  Detail
+                </a>
+              </button>
+            </p>
+          </div>
+        </div>
+      ))}
+      <div id="popup1" className="overlay">
+        <div className="popup">
+          <img src={player.img} />
+          <h2>{player.name}</h2>
+          <a className="close" href="#">
+            &times;
+          </a>
+          <div className="content">{player.info}</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
- 
